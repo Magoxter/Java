b86cd2067831187cd3616a99fee3070916530ba4
Will Xavier <will.lbx@hotmail.com>
2018-05-14 14:23:28 -0400

    Aula #07

---
+++ CursEmVideo/src/Aula07/Funcionario.java
@@ -0,0 +1,24 @@
+package Aula07;
+
+public class Funcionario extends Pessoa {
+	private String setor;
+	private boolean trabalhando;
+	
+	public void mudarTrabalho () {
+		
+	}
+	
+// Getter and Setter
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public boolean isTrabalhando() {
+		return trabalhando;
+	}
+	public void setTrabalhando(boolean trabalhando) {
+		this.trabalhando = trabalhando;
+	}
+}
